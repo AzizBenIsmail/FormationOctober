6d4957a48c711a04b2a4840257331f4f56e478a2
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-10-12 10:34:39 +0100

    création du model user avec hachage

---
+++ models/userschema.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const userSchema = new mongoose.Schema({
+  email: {type : String , required: true , unique: true},
+  password: {type : String , required: true},
+  role: { type: String, enum: ["admin", "moderateur", "client"] },
+  nom: String,
+  prenom: String,
+  age: Number,
+  address: String,
+  image_User : String
+//   image_User : {type : String , required: true , Default:"client.png"}
+});
+
+userSchema.post("save",function (req,res,next){
+    console.log("new user was created & saved successfully");
+    next();
+})
+
+userSchema.pre("save", async function (req,res,next) {
+try {
+    const salt = await bcrypt.genSalt;
+    const user = this;
+    user.password = await bcrypt.hash(user.password, salt);
+    user.image_User = "client.png"
+    user.createdAt = Data.now();
+    next();
+} catch (error) {
+    next(error);
+}
+})
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
