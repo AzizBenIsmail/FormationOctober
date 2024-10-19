64f2094c2a95263d7a1c332ea4d22699ddb2f7db
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-10-19 10:03:55 +0100

    addUserWithImg

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,18 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const userController = require('../controller/userController.js');
-
-/* GET users listing. */
-// router.get('/hello',userController.hello);
-
-// router.get('/', function(req, res, next) {
-//     res.status(200).json('index', { title: 'Express' });
-//   });
+const uploadFile = require('../middlewares/uploadFile.js');
   
 /* GET users  */
 router.get('/getAllUsers',userController.getAllUsers);
 router.delete('/deleteUserByID/:id',userController.deleteUserByID);
 router.post('/addUser',userController.addUser);
+router.post('/addUserWithImg', uploadFile.single("image_User") ,userController.addUserWithImg);
 router.post('/confirmPassword',userController.confirmPassword);
 router.put('/updateUser/:id',userController.updateUser);
 router.get('/getUserById/:id',userController.getUserById);
